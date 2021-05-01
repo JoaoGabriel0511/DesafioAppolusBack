284f57cc4f128a62349a27864c479272ca5ea339
JoaoGabriel0511 <joaogabriel2010@hotmail.com>
2021-05-01 10:40:52 -0300

    feat(account): add account model with a 1:1 relation to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_004708) do
+ActiveRecord::Schema.define(version: 2021_05_01_133834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.float "balance"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
