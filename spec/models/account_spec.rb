de0cdf845fee1d0580d0f6116794f3efe1f3b1a2
JoaoGabriel0511 <joaogabriel2010@hotmail.com>
2021-05-01 11:28:29 -0300

    test(Account): test account model

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  it
+
+  before do
+    @user = User.create(name: "jhon", email: "jhon@email.com", password: "12345678", password_confirmation: "12345678")
+  end
+
+  it "is valid with an user and its balance default value is 0" do
+    account = Account.new(user_id: @user.id)
+    expect(account).to be_valid
+    expect(account.balance).to eq 0.0
+  end
+
+  it "is invalid without an user" do
+    account = Account.new
+    expect(account).to_not be_valid
+  end
+  
+  it "is valid with balance greater or equal to 0"  do
+    account = Account.new(user_id: @user.id, balance: 1.0)
+    expect(account).to be_valid
+  end
+
+  it "is invalid with balance less than 0"  do
+    account = Account.new(user_id: @user.id, balance: -1.0)
+    expect(account).to_not be_valid
+  end
+
+  it "is invalid if user already have another account" do
+    Account.create(user_id: @user.id)
+    account = Account.new(user_id: @user.id)
+    expect(account).to_not be_valid
+  end
+
 end
