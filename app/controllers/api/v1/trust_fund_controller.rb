6985478ef3423126585d3c009407316ed746b157
JoaoGabriel0511 <joaogabriel2010@hotmail.com>
2021-05-07 17:23:57 -0300

    feat(Investment): add invest_value and withdraw_value in trust_fund_controller.rb.

--- app/controllers/api/v1/trust_fund_controller.rb
+++ app/controllers/api/v1/trust_fund_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TrustFundController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_user, only: :create
+  before_action :set_user, except: :index
 
   def index
     trust_funds = TrustFund.all
@@ -18,9 +18,45 @@ class Api::V1::TrustFundController < ApplicationController
     end
   end
 
+  def invest_value
+    investment = Investment.find_or_initialize_by(trust_fund_id: params[:investment][:trust_fund_id], user: @userAccount[:user])
+    if investment
+      @userAccount[:account].withdraw(params[:investment][:value])
+      investment.deposit(params[:investment][:value])
+      if @userAccount[:account].save
+        investment.save
+        render json: {data:{ user: @userAccount[:user], account: @userAccount[:account], investment: investment }, message: "value invested"}, status: :ok
+      else
+        render json: {errors: "Cant invest greater the user account balance"}, status: :unprocessable_entity
+      end
+    else
+      render json: {errors: investment.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def withdraw_value
+      investment = Investment.find_or_initialize_by(trust_fund_id: params[:investment][:trust_fund_id], user: @userAccount[:user])
+      if investment
+        @userAccount[:account].deposit(params[:investment][:value])
+        investment.withdraw(params[:investment][:value])
+        if investment.save
+          @userAccount[:account].save
+          render json: {data:{ user: @userAccount[:user], account: @userAccount[:account], investment: investment }, message: "value invested"}, status: :ok
+        else
+          render json: {errors: "Cant withdraw a value greater then the investment value"}, status: :unprocessable_entity
+        end
+      else
+        render json: {errors: investment.errors}, status: :unprocessable_entity
+      end
+  end
+
   private
     def trust_fund_params
       params.require(:trust_fund).permit(:name, :fund_type)
+    end
+
+    def investment_params
+      params.require(:investment).permit(:value, :trust_fund_id)
     end
 
     def set_user
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,5 +1,14 @@
 class Investment < ApplicationRecord
   belongs_to :user
   belongs_to :trust_fund
+  validates :value, numericality: {greater_than_or_equal_to: 0}
   validates :user_id, uniqueness: { scope: :trust_fund_id }
+
+  def deposit(value)
+    self.value += value
+  end
+
+  def withdraw(value)
+    self.value -= value
+  end
 end
